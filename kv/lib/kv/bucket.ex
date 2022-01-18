3c5a0c554ed9584929f02b0e959a0e98f9f8ecab
hvieira <hugo.r.f.s.vieira@gmail.com>
2022-01-18 16:44:34 +0000

    Add initial mix&OTP guide bit

---
+++ kv/lib/kv/bucket.ex
@@ -0,0 +1,37 @@
+defmodule KV.Bucket do
+  use Agent
+
+  @doc """
+  Starts a new bucket.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{} end)
+  end
+
+  @doc """
+  Gets a value from the `bucket` by `key`.
+  """
+  @spec get(pid(), String.t()) :: term
+  def get(bucket, key) do
+    Agent.get(bucket, &Map.get(&1, key))
+  end
+
+  @doc """
+  Puts the `value` for the given `key` in the `bucket`.
+  """
+  @spec put(pid(), String.t(), term) :: term
+  def put(bucket, key, value) do
+    Agent.update(bucket, &Map.put(&1, key, value))
+  end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  @spec delete(pid(), String.t()) :: term
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
+
+end
