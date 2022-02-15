623fcf2ecb730f955c6b1f77708be394c4aac1b3
hvieira <hugo.r.f.s.vieira@gmail.com>
2022-02-15 09:10:43 +0000

    Name improvements

--- calculator_app/apps/calculator_core/lib/calculator/core/resolver.ex
+++ calculator_app/apps/calculator_core/lib/calculator/core/resolver.ex
@@ -1,68 +1,75 @@
 defmodule Calculator.Core.Resolver do
   alias Calculator.Core.Expression
 
+  def resolve(expr) do
+    expr
+    |> resolve_expression
+  end
+
+  defp resolve_expression(n) when is_number(n),
+       do: n
+
   ### add ###
-  def resolve_expression(%Expression{left: l, operator: :add, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :add, right: r})
       when is_number(l) and is_number(r),
       do: l + r
 
-  def resolve_expression(%Expression{left: l, operator: :add, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :add, right: r})
       when is_number(l),
       do: l + resolve_expression(r)
 
-  def resolve_expression(%Expression{left: l, operator: :add, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :add, right: r})
       when is_number(r),
       do: resolve_expression(l) + r
 
-  def resolve_expression(%Expression{left: l, operator: :add, right: r}),
+  defp resolve_expression(%Expression{left: l, operator: :add, right: r}),
     do: resolve_expression(l) + resolve_expression(r)
 
   ### subtract ###
-  def resolve_expression(%Expression{left: l, operator: :subtract, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :subtract, right: r})
       when is_number(l) and is_number(r),
       do: l - r
 
-  def resolve_expression(%Expression{left: l, operator: :subtract, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :subtract, right: r})
       when is_number(l),
       do: l - resolve_expression(r)
 
-  def resolve_expression(%Expression{left: l, operator: :subtract, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :subtract, right: r})
       when is_number(r),
       do: resolve_expression(l) - r
 
-  def resolve_expression(%Expression{left: l, operator: :subtract, right: r}),
+  defp resolve_expression(%Expression{left: l, operator: :subtract, right: r}),
     do: resolve_expression(l) - resolve_expression(r)
 
   ### multiply ###
-  def resolve_expression(%Expression{left: l, operator: :multiply, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :multiply, right: r})
       when is_number(l) and is_number(r),
       do: l * r
 
-  def resolve_expression(%Expression{left: l, operator: :multiply, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :multiply, right: r})
       when is_number(l),
       do: l * resolve_expression(r)
 
-  def resolve_expression(%Expression{left: l, operator: :multiply, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :multiply, right: r})
       when is_number(r),
       do: resolve_expression(l) * r
 
-  def resolve_expression(%Expression{left: l, operator: :multiply, right: r}),
+  defp resolve_expression(%Expression{left: l, operator: :multiply, right: r}),
     do: resolve_expression(l) * resolve_expression(r)
 
   ### divide ###
-  def resolve_expression(%Expression{left: l, operator: :divide, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :divide, right: r})
       when is_number(l) and is_number(r),
       do: l / r
 
-  def resolve_expression(%Expression{left: l, operator: :divide, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :divide, right: r})
       when is_number(l),
       do: l / resolve_expression(r)
 
-  def resolve_expression(%Expression{left: l, operator: :divide, right: r})
+  defp resolve_expression(%Expression{left: l, operator: :divide, right: r})
       when is_number(r),
       do: resolve_expression(l) / r
 
-  def resolve_expression(%Expression{left: l, operator: :divide, right: r}),
-      do: resolve_expression(l) / resolve_expression(r)
-
+  defp resolve_expression(%Expression{left: l, operator: :divide, right: r}),
+    do: resolve_expression(l) / resolve_expression(r)
 end
--- calculator_app/apps/calculator_core/test/calculator/core/calculator_resolver_test.exs
+++ calculator_app/apps/calculator_core/test/calculator/core/calculator_resolver_test.exs
@@ -6,25 +6,25 @@ defmodule CalculatorCoreResolverTest do
   doctest Resolver
 
   test "Can simple expressions" do
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 3,
              operator: :add,
              right: 7
            }) == 10
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 3,
              operator: :subtract,
              right: 5
            }) == -2
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 1,
              operator: :multiply,
              right: 1
            }) == 1
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 5,
              operator: :divide,
              right: 2
@@ -32,19 +32,19 @@ defmodule CalculatorCoreResolverTest do
   end
 
   test "Can resolve expressions that have multiple consecutive operations - add" do
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 7, operator: :add, right: 6},
              operator: :add,
              right: 1
            }) == 14
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 1,
              operator: :add,
              right: %Expression{left: 2, operator: :add, right: 3}
            }) == 6
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 3.81, operator: :add, right: 3.81},
              operator: :add,
              right: %Expression{left: 3.81, operator: :add, right: 3.81}
@@ -52,19 +52,19 @@ defmodule CalculatorCoreResolverTest do
   end
 
   test "Can resolve expressions that have multiple consecutive operations - subtract" do
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 7, operator: :subtract, right: 6},
              operator: :subtract,
              right: 1
            }) == 0
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 1,
              operator: :subtract,
              right: %Expression{left: 2, operator: :subtract, right: 3}
            }) == 2
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 3.81, operator: :subtract, right: 3.81},
              operator: :subtract,
              right: %Expression{left: 3.81, operator: :subtract, right: 3.81}
@@ -72,19 +72,19 @@ defmodule CalculatorCoreResolverTest do
   end
 
   test "Can resolve expressions that have multiple consecutive operations - multiply" do
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 7, operator: :multiply, right: 6},
              operator: :multiply,
              right: 1
            }) == 42
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 1,
              operator: :multiply,
              right: %Expression{left: 2, operator: :multiply, right: 3}
            }) == 6
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 3.81, operator: :multiply, right: 3.81},
              operator: :multiply,
              right: %Expression{left: 3.81, operator: :multiply, right: 3.81}
@@ -92,22 +92,81 @@ defmodule CalculatorCoreResolverTest do
   end
 
   test "Can resolve expressions that have multiple consecutive operations - divide" do
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 7, operator: :divide, right: 6},
              operator: :divide,
              right: 1
-           }) == (7/6)/1
+           }) == 7 / 6 / 1
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: 1,
              operator: :divide,
              right: %Expression{left: 2, operator: :divide, right: 3}
-           }) == 1/(2/3)
+           }) == 1 / (2 / 3)
 
-    assert Resolver.resolve_expression(%Expression{
+    assert Resolver.resolve(%Expression{
              left: %Expression{left: 3.81, operator: :divide, right: 3.81},
              operator: :divide,
              right: %Expression{left: 3.81, operator: :divide, right: 3.81}
            }) == 1
+  end
+
+  test "Resolves expressions by resolving whatever is within parentheses first" do
+    assert Resolver.resolve(%Expression{
+             within_parens: true,
+             left: 1,
+             operator: :add,
+             right: 1
+           }) == 2
+
+    assert Resolver.resolve(%Expression{
+             left: %Expression{within_parens: true, left: 3, operator: :subtract, right: 1},
+             operator: :multiply,
+             right: 100
+           }) == 200
+
+    assert Resolver.resolve(%Expression{
+             left: 1,
+             operator: :divide,
+             right: %Expression{within_parens: true, left: 3, operator: :subtract, right: 1}
+           }) == 1 / 2
+
+    assert Resolver.resolve(%Expression{
+             left: %Expression{within_parens: true, left: 2, operator: :multiply, right: 100},
+             operator: :divide,
+             right: %Expression{within_parens: true, left: 400, operator: :divide, right: 2}
+           }) == 1
+
+    assert Resolver.resolve(%Expression{
+             left: %Expression{left: 1, operator: :add, right: 2},
+             operator: :divide,
+             right: %Expression{within_parens: true, left: 1, operator: :subtract, right: 6}
+           }) == 3 / (1 - 6)
+  end
+
+  test "Resolves expressions by resolving whatever is within parentheses first - nested " do
+    assert Resolver.resolve(%Expression{
+             within_parens: true,
+             left: 1,
+             operator: :add,
+             right: %Expression{
+               left: 2,
+               operator: :multiply,
+               right: %Expression{
+                 within_parens: true,
+                 left: 1,
+                 operator: :add,
+                 right: %Expression{
+                   left: 1,
+                   operator: :add,
+                   right: %Expression{
+                     left: 1,
+                     operator: :add,
+                     right: 1
+                   }
+                 }
+               }
+             }
+           }) == (1 + 2 * (1 + 1 + 1 + 1))
   end
 end
