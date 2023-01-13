10aeebc6190efb4445bd5cd77275da8b70d8306d
Hugo Vieira <hugo.r.f.s.vieira@gmail.com>
2023-01-13 17:08:48 +0000

    add tests to verify signatures

--- dummy_product_api_umbrella/apps/dummy_product_api_web/test/dummy_product_api_web/auth/jwt_test.exs
+++ dummy_product_api_umbrella/apps/dummy_product_api_web/test/dummy_product_api_web/auth/jwt_test.exs
@@ -15,4 +15,24 @@ defmodule DummyProductApiWeb.Auth.JWTTest do
     assert_in_delta claims["iat"], :os.system_time(:seconds), 10
   end
 
+  test "can verify signatures from generated jwts" do
+    {:ok, token_str, generated_claims} = JWT.generate_and_sign()
+
+    {:ok, claims} = JWT.verify(token_str)
+
+    assert claims == generated_claims
+  end
+
+  test "can verify jwts with bad signature" do
+    bad_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJhdWQiOiJkdW1teV9wcm9kdWN0LmNvbSIsImlzcyI6ImR1bW15X3Byb2R1Y3QuY29tIn0.4GPy1nvnqkjiE-rCJ5Yml0ZIPBAx5J9LHpY9G16LVA8"
+
+    {:error, error_msg} = JWT.verify(bad_token)
+
+    assert error_msg == :signature_error
+  end
+
+  # TODO verify and/or validate audience
+
+  # TODO not before and expiration tests to be done via https://hexdocs.pm/joken/Joken.html#current_time/0
+
 end
