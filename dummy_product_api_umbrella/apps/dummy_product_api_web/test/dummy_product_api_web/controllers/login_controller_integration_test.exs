36e60bc7e881fabc8bfc0ac8acbc84a0d304023e
Hugo Vieira <hugo.r.f.s.vieira@gmail.com>
2023-05-31 15:36:31 +0100

    remove unnecessary commented code

--- dummy_product_api_umbrella/apps/dummy_product_api_web/test/dummy_product_api_web/controllers/login_controller_integration_test.exs
+++ dummy_product_api_umbrella/apps/dummy_product_api_web/test/dummy_product_api_web/controllers/login_controller_integration_test.exs
@@ -3,8 +3,6 @@ defmodule DummyProductApiWeb.LoginControllerIntegrationTest do
 
   @moduletag :integration
 
-  # @uuid_regex ~r/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/
-
   alias DummyProductApiWeb.Auth.JWT
 
   test "user log in", %{conn: conn} do
@@ -28,7 +26,6 @@ defmodule DummyProductApiWeb.LoginControllerIntegrationTest do
       }
     } = json_response(response, 200)
 
-    # validate that the returned token is valid and verifiable and has the correct claims
     assert match?({:ok, %{"sub" => ^user_id}}, JWT.verify_and_validate(token_str, :new_signer))
   end
 end
