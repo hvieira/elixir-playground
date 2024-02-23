d289a201318290e751ddcdcadd8ffa5a678a0804
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-02-23 15:22:28 +0000

    formatted project

--- minesweeper_umbrella/apps/minesweeper_web/test/minesweeper_web/controllers/error_html_test.exs
+++ minesweeper_umbrella/apps/minesweeper_web/test/minesweeper_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule MinesweeperWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(MinesweeperWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(MinesweeperWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
