5cd4cbda82600978e022476e88011260a1184fd7
hvieira <hugo.r.f.s.vieira@gmail.com>
2024-02-27 10:52:08 +0000

    add revealing cells behaviour

--- minesweeper_umbrella/apps/minesweeper/lib/minesweeper/game.ex
+++ minesweeper_umbrella/apps/minesweeper/lib/minesweeper/game.ex
@@ -26,6 +26,75 @@ defmodule Minesweeper.Game do
       cells: create_cells(width, height, number_of_mines, mine_picker_func)
     }
 
+  def reveal(%Minesweeper.Game{} = game, coordinates),
+    do: reveal_cell(game, coordinates, Map.get(game.cells, coordinates))
+
+  defp reveal_cell(game, _coordinates, nil), do: {:invalid_coordinates, game}
+
+  defp reveal_cell(game, _coordinates, %Minesweeper.Cell{revealed: true, mined: false}),
+    do: {:ok, game}
+
+  defp reveal_cell(game, coordinates, %Minesweeper.Cell{
+         mined: true
+       }),
+       do:
+         {:lost,
+          %{
+            game
+            | cells: Map.update!(game.cells, coordinates, fn cell -> %{cell | revealed: true} end)
+          }}
+
+  defp reveal_cell(game, coordinates, %Minesweeper.Cell{
+         revealed: false,
+         mined: false,
+         num_adjacent_mines: mines
+       })
+       when mines > 0 do
+    {:ok,
+     %{
+       game
+       | cells: Map.update!(game.cells, coordinates, fn cell -> %{cell | revealed: true} end)
+     }}
+  end
+
+  defp reveal_cell(game, coordinates, %Minesweeper.Cell{
+         revealed: false,
+         mined: false,
+         num_adjacent_mines: mines
+       })
+       when mines == 0 do
+    {:ok, reveal_all_safe_cells(game, [coordinates])}
+  end
+
+  defp reveal_all_safe_cells(game_state, coordinates_to_explore, explored \\ MapSet.new())
+  defp reveal_all_safe_cells(game_state, [], _explored), do: game_state
+
+  defp reveal_all_safe_cells(game_state, [current_coordinates | t], explored) do
+    case MapSet.member?(explored, current_coordinates) do
+      true ->
+        reveal_all_safe_cells(game_state, t, explored)
+
+      false ->
+        case Map.get(game_state.cells, current_coordinates) do
+          %Minesweeper.Cell{num_adjacent_mines: mines} when mines == 0 ->
+            reveal_all_safe_cells(
+              %{
+                game_state
+                | cells:
+                    Map.update!(game_state.cells, current_coordinates, fn cell ->
+                      %{cell | revealed: true}
+                    end)
+              },
+              Enum.concat(t, get_adjacent_cells(current_coordinates, game_state.cells)),
+              MapSet.put(explored, current_coordinates)
+            )
+
+          _ ->
+            reveal_all_safe_cells(game_state, t, MapSet.put(explored, current_coordinates))
+        end
+    end
+  end
+
   defp create_cells(width, height, number_of_mines, mine_picker_func) do
     unmined_field =
       for x <- 0..(height - 1), y <- 0..(width - 1), into: %{} do
@@ -57,13 +126,14 @@ defmodule Minesweeper.Game do
     |> Enum.count(fn coordinates -> Map.get(field, coordinates).mined end)
   end
 
-  defp get_adjacent_cells(target_coordinates, field) do
+  defp get_adjacent_cells(target_coordinates, game_cells) do
     for x <- (target_coordinates.x - 1)..(target_coordinates.x + 1),
         y <- (target_coordinates.y - 1)..(target_coordinates.y + 1),
-        Map.has_key?(field, %Minesweeper.Coordinates{x: x, y: y}),
+        Map.has_key?(game_cells, %Minesweeper.Coordinates{x: x, y: y}),
         into: MapSet.new() do
       %Minesweeper.Coordinates{x: x, y: y}
     end
+    # remove the target_coordinates from the set (its the adjacent cells and should not include the target ones)
     |> MapSet.delete(target_coordinates)
   end
 end
--- minesweeper_umbrella/apps/minesweeper/test/minesweeper_test.exs
+++ minesweeper_umbrella/apps/minesweeper/test/minesweeper_test.exs
@@ -139,4 +139,257 @@ defmodule Minesweeper.GameTest do
              }
            }
   end
+
+  test "revealing a non-mined cell with adjacent mines reveals only that cell" do
+    game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+      }
+    }
+
+    assert Game.reveal(game, %Coordinates{x: 0, y: 0}) ==
+             {:ok,
+              %Game{
+                width: 3,
+                height: 3,
+                cells: %{
+                  %Coordinates{x: 0, y: 0} => %Cell{
+                    revealed: true,
+                    mined: false,
+                    num_adjacent_mines: 1
+                  },
+                  %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+                  %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+                }
+              }}
+
+    assert Game.reveal(game, %Coordinates{x: 0, y: 2}) ==
+             {:ok,
+              %Game{
+                width: 3,
+                height: 3,
+                cells: %{
+                  %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 2} => %Cell{
+                    revealed: true,
+                    mined: false,
+                    num_adjacent_mines: 2
+                  },
+                  %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+                  %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+                }
+              }}
+
+    assert Game.reveal(game, %Coordinates{x: 2, y: 1}) ==
+             {:ok,
+              %Game{
+                width: 3,
+                height: 3,
+                cells: %{
+                  %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+                  %Coordinates{x: 2, y: 1} => %Cell{
+                    revealed: true,
+                    mined: false,
+                    num_adjacent_mines: 1
+                  },
+                  %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+                }
+              }}
+  end
+
+  test "revealing a revealed cell is a no-op" do
+    game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{
+          revealed: true,
+          mined: false,
+          num_adjacent_mines: 1
+        },
+        %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+      }
+    }
+
+    assert Game.reveal(game, %Coordinates{x: 2, y: 1}) == {:ok, game}
+  end
+
+  test "revealing a cell with invalid coordinates is an error" do
+    game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+      }
+    }
+
+    assert Game.reveal(game, %Coordinates{x: 0, y: 3}) == {:invalid_coordinates, game}
+    assert Game.reveal(game, %Coordinates{x: 3, y: 0}) == {:invalid_coordinates, game}
+    assert Game.reveal(game, %Coordinates{x: 3, y: 3}) == {:invalid_coordinates, game}
+    assert Game.reveal(game, %Coordinates{x: 7, y: 3}) == {:invalid_coordinates, game}
+    assert Game.reveal(game, %Coordinates{x: 3, y: 7}) == {:invalid_coordinates, game}
+    assert Game.reveal(game, %Coordinates{x: 7, y: 7}) == {:invalid_coordinates, game}
+  end
+
+  test "revealing a mined cell is game over" do
+    game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+      }
+    }
+
+    assert Game.reveal(game, %Coordinates{x: 1, y: 2}) ==
+             {:lost,
+              %Game{
+                width: 3,
+                height: 3,
+                cells: %{
+                  %Coordinates{x: 0, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 2} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 0} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 1, y: 1} => %Cell{mined: false, num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 2} => %Cell{
+                    revealed: true,
+                    mined: true,
+                    num_adjacent_mines: 1
+                  },
+                  %Coordinates{x: 2, y: 0} => %Cell{mined: false, num_adjacent_mines: 0},
+                  %Coordinates{x: 2, y: 1} => %Cell{mined: false, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 2} => %Cell{mined: false, num_adjacent_mines: 1}
+                }
+              }}
+  end
+
+  test "revealing a safe (not mined & no adjacent mines) cell reveals that cell and all other safe cells recursively - scenario with only 1 cell" do
+    game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 0} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{num_adjacent_mines: 2},
+        %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 0} => %Cell{num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 2, y: 2} => %Cell{num_adjacent_mines: 1}
+      }
+    }
+
+    assert Game.reveal(game, %Coordinates{x: 2, y: 0}) ==
+             {:ok,
+              %Game{
+                width: 3,
+                height: 3,
+                cells: %{
+                  %Coordinates{x: 0, y: 0} => %Cell{num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 1} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 0, y: 2} => %Cell{num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 0} => %Cell{num_adjacent_mines: 1},
+                  %Coordinates{x: 1, y: 1} => %Cell{num_adjacent_mines: 2},
+                  %Coordinates{x: 1, y: 2} => %Cell{mined: true, num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 0} => %Cell{
+                    revealed: true,
+                    mined: false,
+                    num_adjacent_mines: 0
+                  },
+                  %Coordinates{x: 2, y: 1} => %Cell{num_adjacent_mines: 1},
+                  %Coordinates{x: 2, y: 2} => %Cell{num_adjacent_mines: 1}
+                }
+              }}
+  end
+
+  test "revealing a safe (not mined & no adjacent mines) cell reveals that cell and all other safe cells recursively - scenario with more cells" do
+    initial_game = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: true, num_adjacent_mines: 0},
+        %Coordinates{x: 0, y: 1} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{num_adjacent_mines: 0},
+        %Coordinates{x: 1, y: 0} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 2} => %Cell{num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 0} => %Cell{num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 2} => %Cell{num_adjacent_mines: 0}
+      }
+    }
+
+    all_safe_cells_revealed = %Game{
+      width: 3,
+      height: 3,
+      cells: %{
+        %Coordinates{x: 0, y: 0} => %Cell{mined: true, num_adjacent_mines: 0},
+        %Coordinates{x: 0, y: 1} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 0, y: 2} => %Cell{revealed: true, num_adjacent_mines: 0},
+        %Coordinates{x: 1, y: 0} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 1} => %Cell{num_adjacent_mines: 1},
+        %Coordinates{x: 1, y: 2} => %Cell{revealed: true, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 0} => %Cell{revealed: true, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 1} => %Cell{revealed: true, num_adjacent_mines: 0},
+        %Coordinates{x: 2, y: 2} => %Cell{revealed: true, num_adjacent_mines: 0}
+      }
+    }
+
+    assert Game.reveal(initial_game, %Coordinates{x: 0, y: 2}) == {:ok, all_safe_cells_revealed}
+    assert Game.reveal(initial_game, %Coordinates{x: 1, y: 2}) == {:ok, all_safe_cells_revealed}
+    assert Game.reveal(initial_game, %Coordinates{x: 2, y: 0}) == {:ok, all_safe_cells_revealed}
+    assert Game.reveal(initial_game, %Coordinates{x: 2, y: 1}) == {:ok, all_safe_cells_revealed}
+    assert Game.reveal(initial_game, %Coordinates{x: 2, y: 2}) == {:ok, all_safe_cells_revealed}
+  end
 end
